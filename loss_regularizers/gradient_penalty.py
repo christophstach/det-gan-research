Fix for msg gan Add noise normalization

loss_regularizers/gradient_penalty.py
@@ -50,7 +50,10 @@ class GradientPenalty(loss_regularizers.base.LossRegularizer):
             create_graph=True
         )
 
-        inputs_gradients = [input_gradients.view(input_gradients.shape[0], -1) for input_gradients in inputs_gradients]
+        inputs_gradients = [
+            input_gradients.view(input_gradients.shape[0], -1)
+            for input_gradients in inputs_gradients
+        ]
         gradients = torch.cat(inputs_gradients, dim=1)
 
         if self.norm_type == "l1":

utils/sample_noise.py
@@ -1,7 +1,11 @@
 import torch
 
 
-def sample_noise(batch_size: int, noise_size: int):
+def sample_noise(batch_size: int, noise_size: int, normalize=True):
     # Could add truncation trick here
+    noise = torch.rand(size=(batch_size, noise_size))
 
-    return torch.rand(size=(batch_size, noise_size))
+    if normalize:
+        return noise / noise.norm(dim=-1, keepdim=True) * (noise_size ** 0.5)
+    else:
+        return noise