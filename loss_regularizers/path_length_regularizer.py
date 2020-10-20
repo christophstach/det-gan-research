eb43cfc041fbc57f35de916494b9f5b5ca826a6d
Christoph Stach <christoph.stach@gmail.com>
2020-10-20 16:14:14 +0800

    plr doesn't return last calculated_pp anymore

--- loss_regularizers/path_length_regularizer.py
+++ loss_regularizers/path_length_regularizer.py
@@ -24,7 +24,6 @@ class PathLengthRegularizer(loss_regularizers.base.LossRegularizer):
         self.__lazy_regularization_interval = lazy_regularization_interval
         self.__steps = 0
         self.__first_step = first_step
-        self.__last_calculated_pp = None
 
     def __call__(self, w: torch.Tensor, real_images: List[torch.Tensor], fake_images: List[torch.Tensor]):
         if self.__steps >= self.__first_step and self.__steps % self.__lazy_regularization_interval == 0:
@@ -50,10 +49,9 @@ class PathLengthRegularizer(loss_regularizers.base.LossRegularizer):
                 self.__moving_mean_path_length = path_lengths_mean
 
             path_penalty = self.__weight * ((path_lengths - self.__moving_mean_path_length) ** 2).mean()
-            self.__last_calculated_pp = path_penalty.clone().detach()
             self.__steps += 1
 
             return path_penalty
         else:
             self.__steps += 1
-            return self.__last_calculated_pp
+            return None
