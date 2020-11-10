Fix error in gp calculation

loss_regularizers/gradient_penalty.py
@@ -15,7 +15,7 @@ class GradientPenalty(loss_regularizers.base.LossRegularizer):
                  power: int = 2,
                  norm_type: str = "l2",
                  penalty_type: str = "ls",
-                 lazy_regularization_interval: int = 4) -> None:
+                 lazy_regularization_interval: int = 1) -> None:
 
         super().__init__()
 
@@ -37,7 +37,7 @@ class GradientPenalty(loss_regularizers.base.LossRegularizer):
         return interpolation
 
     def __call__(self, w: torch.Tensor, real_images: List[torch.Tensor], fake_images: List[torch.Tensor]):
-        if self.coefficient > 0.0 and self.steps % self.lazy_regularization_interval:
+        if self.coefficient > 0.0 and self.steps % self.lazy_regularization_interval == 0:
             alpha = torch.rand(real_images[0].shape[0], 1, 1, 1)
             alpha = self.context.to_device(alpha)
 