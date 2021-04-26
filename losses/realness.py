4ceaf274566cc0072191f8e5b2e87401223df1a0
Christoph Stach <christoph.stach@gmail.com>
2021-04-26 18:43:23 +0800

    WIP

--- losses/realness.py
+++ losses/realness.py
@@ -1,13 +1,24 @@
 import torch
-from numpy import histogram, array
-from scipy.stats import skewnorm
+from numpy import histogram, random
 from torch import Tensor, from_numpy
 
 import losses.base.loss
 
 
-def kl_div(p, q):
-    return torch.mean(torch.sum(p * (p / q).log(), dim=1))
+def js_div(p, q, reduce=True):
+    m = 0.5 * (p + q)
+    jsd = 0.5 * (kl_div(p, m, reduce=False) + kl_div(q, m, reduce=False))
+
+    return torch.mean(jsd) if reduce else jsd
+
+
+def kl_div(p, q, reduce=True):
+    kld = torch.sum(
+        p * (p / q).log(),
+        dim=1
+    )
+
+    return torch.mean(kld) if reduce else kld
 
 
 class Realness(losses.base.Loss):
@@ -16,21 +27,21 @@ class Realness(losses.base.Loss):
 
         self.score_dim = score_dim
 
-        # gauss = random.normal(0.0, 0.1, size=10000)
-        # count, _ = histogram(gauss, self.score_dim)
-        # self.anchor0 = from_numpy(count / sum(count)).float()
-
-        # uniform = random.uniform(-1.0, 1.0, size=10000)
-        # count, _ = histogram(uniform, self.score_dim)
-        # self.anchor1 = from_numpy(count / sum(count)).float()
-
-        skew_left = skewnorm.rvs(-5, size=10000)
-        count, _ = histogram(skew_left, self.score_dim)
+        gauss = random.normal(0.0, 0.1, size=10000)
+        count, _ = histogram(gauss, self.score_dim)
         self.anchor0 = from_numpy(count / sum(count)).float()
 
-        skew_right = skewnorm.rvs(5, size=10000)
-        count, _ = histogram(skew_right, self.score_dim)
+        uniform = random.uniform(-1.0, 1.0, size=10000)
+        count, _ = histogram(uniform, self.score_dim)
         self.anchor1 = from_numpy(count / sum(count)).float()
+
+        # skew_left = skewnorm.rvs(-5, size=10000)
+        # count, _ = histogram(skew_left, self.score_dim)
+        # self.anchor0 = from_numpy(count / sum(count)).float()
+
+        # skew_right = skewnorm.rvs(5, size=10000)
+        # count, _ = histogram(skew_right, self.score_dim)
+        # self.anchor1 = from_numpy(count / sum(count)).float()
 
     def discriminator_loss(self, real_scores: Tensor, fake_scores: Tensor) -> Tensor:
         self.anchor0 = self.anchor0.to(real_scores.device)
